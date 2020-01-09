099670b9e80d3d4144e0e34dc73be8344eab1974
emmschuster <emmschuster@tsn.at>
2020-01-09 10:39:22 +0100

    bug fixxes

--- AllMain.java
+++ AllMain.java
@@ -16,71 +16,71 @@ public class AllMain {
 			laengeArray = sc.nextInt();
 			ranArray = new int[laengeArray];
 			fillArray(ranArray);
+			System.out.println("\nDein Array : ");
+			for (int i = 0; i < ranArray.length; i++) {
+				System.out.print(ranArray[i] + "	");
+			}
+			System.out.println("\n");
+//			System.out.println("hallo1");
+			System.out.println(
+					"Mithilfe welchem Verfahren soll er sortiert werden?\nBubblesort = 1\nBogosort = 2\nSelectionsort = 3\nInsertionsort = 4\nQuicksort = 5");
+			try {
+				sortierArt = sc.nextInt();
+			} catch (Exception e) {
+				System.out.println("Es dürfen nur Zahlen eingeben werden!!!");
+			}
+			System.out.println("");
+			if (sortierArt > 5 || sortierArt < 1) {
+				System.out.println("Die Zahl muss zwischen 1 und 5 liegen!!!");
+			} else {
+				switch (sortierArt) {
+				case 1:
+					long abbs = System.nanoTime();
+					Bubblesort bbs = new Bubblesort(ranArray);
+					bbs.print();
+					long ebbs = System.nanoTime();
+					zeitausgabe(abbs, ebbs);
+//				System.out.println("hallo2");
+					break;
+				case 2:
+					long abs = System.nanoTime();
+					Bogosort bs = new Bogosort(ranArray);
+					bs.print();
+					long ebs = System.nanoTime();
+					zeitausgabe(abs, ebs);
+					break;
+				case 3:
+					long ass = System.nanoTime();
+					Selectionsort ss = new Selectionsort(ranArray);
+					ss.print();
+					long ess = System.nanoTime();
+					zeitausgabe(ass, ess);
+					break;
+				case 4:
+					long ais = System.nanoTime();
+					Insertionsort is = new Insertionsort(ranArray);
+					is.print();
+					long eis = System.nanoTime();
+					zeitausgabe(ais, eis);
+					break;
+				case 5: 
+					long aqs =System.nanoTime();
+					Quicksort qs=new Quicksort(ranArray);
+					qs.print();
+					long eqs= System.nanoTime();
+					zeitausgabe(aqs, eqs);
+					break;
+				}
+			}
 		} catch (Exception e) {
 			System.out.println("Es dürfen nur Zahlen höher 0 eingeben werden!!!");
-		}
-		System.out.println("\nDein Array : ");
-		for (int i = 0; i < ranArray.length; i++) {
-			System.out.print(ranArray[i] + "	");
-		}
-		System.out.println("\n");
-//		System.out.println("hallo1");
-		System.out.println(
-				"Mithilfe welchem Verfahren soll er sortiert werden?\nBubblesort = 1\nBogosort = 2\nSelectionsort = 3\nInsertionsort = 4\nQuicksort = 5");
-		try {
-			sortierArt = sc.nextInt();
-		} catch (Exception e) {
-			System.out.println("Es dürfen nur Zahlen eingeben werden!!!");
-		}
-		System.out.println("");
-		if (sortierArt > 5 || sortierArt < 1) {
-			System.out.println("Die Zahl muss zwischen 1 und 5 liegen!!!");
-		} else {
-			switch (sortierArt) {
-			case 1:
-				long abbs = System.nanoTime();
-				Bubblesort bbs = new Bubblesort(ranArray);
-				bbs.print();
-				long ebbs = System.nanoTime();
-				zeitausgabe(abbs, ebbs);
-//			System.out.println("hallo2");
-				break;
-			case 2:
-				long abs = System.nanoTime();
-				Bogosort bs = new Bogosort(ranArray);
-				bs.print();
-				long ebs = System.nanoTime();
-				zeitausgabe(abs, ebs);
-				break;
-			case 3:
-				long ass = System.nanoTime();
-				Selectionsort ss = new Selectionsort(ranArray);
-				ss.print();
-				long ess = System.nanoTime();
-				zeitausgabe(ass, ess);
-				break;
-			case 4:
-				long ais = System.nanoTime();
-				Insertionsort is = new Insertionsort(ranArray);
-				is.print();
-				long eis = System.nanoTime();
-				zeitausgabe(ais, eis);
-				break;
-			case 5: 
-				long aqs =System.nanoTime();
-				Quicksort qs=new Quicksort(ranArray);
-				qs.print();
-				long eqs= System.nanoTime();
-				zeitausgabe(aqs, eqs);
-				break;
-			}
 		}
 		sc.close();
 	}
 
 	private static void zeitausgabe(long abbs, long ebbs) {
 		long zeit = ebbs - abbs;
-		System.out.println("Es hat " + zeit/1000/1000 + " Sekunden gedauert.\nDas waren "+zeit/1000+" milli sec und "+zeit+" nano sec");
+		System.out.println("Es hat " + zeit/100000000.0 + " Sekunden gedauert.\nDas waren "+zeit/1000000.0 +" milli sec und "+zeit+" nano sec");
 	}
 
 	private static void fillArray(int[] x) {
